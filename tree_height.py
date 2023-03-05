4a1b7081a3d876c57deaa333a886220b8b7ec242
Rinalds Ulmanis 221RDB152 <118060249+Rinalds13@users.noreply.github.com>
2023-03-05 23:54:26 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,30 +1,67 @@
+# 221RDB152 Rinalds Ulmanis 7. grupa 
 # python3
 
 import sys
 import threading
 
+def lasa_input():
+    
+    # kāds būs ievades veids
+    ievades_veids = input("Ievades veids:")
+    while ievades_veids not in ["I", "F"]:
+        # ja nav ne I ne F
+        ievades_veids = input("Nepareizs input type")
 
+    # ja ievades veids ir I
+    if ievades_veids == "I":
+        n = int(input())
+        parents = list(map(int, input().split()))
+    # citādāk jāievada faila nosaukums
+    else:
+        nosaukums = input()
+        # nevar saturēt burtu a
+        while "a" in nosaukums:
+            nosaukums = input("Nepareizs nosaukums")
+        # meiģina atvērt failu
+        try:
+            with open(f"folder/{nosaukums}", "r") as fails:
+                n = int(fails.readline())
+                parents = list(map(int, fails.readline().split()))
+        # ja nesanāk nolasīt failu
+        except:
+            print("error faila lasīšanā")
+            return lasa_input()
+
+    return n, parents
+
+# apreiķina augstumu
 def compute_height(n, parents):
-    # Write this function
+    ch = [[] for _ in range(n)]
+    r = None
+
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
+            r = i
+        else:
+            ch[parent].append(i)
+
+    stack = [(r, 0)]
     max_height = 0
-    # Your code here
+
+    while stack:
+        n, height = stack.pop()
+        max_height = max(max_height, height)
+
+        for c in ch[n]:
+            stack.append((c, height+1))
     return max_height
 
-
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    n, parents = lasa_input()
+    height = compute_height(n, parents)
+    print(height + 1)
 
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
